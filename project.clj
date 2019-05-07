5c7fe10c94e5b14229d0b2b8a4d29dc232be4a32
Ryan Spangler <spanglry@stanford.edu>
2019-05-06 17:36:08 -0700

    starting on sisyphus

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject sisyphus "0.0.1"
-  :description "Eterally execute tasks"
+  :description "Eternally execute tasks"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,4 +8,5 @@
                  [com.novemberain/langohr "5.1.0"]
                  [com.google.cloud/google-cloud-storage "1.70.0"]
                  [com.spotify/docker-client "8.15.2"]
-                 [spootnik/kinsky "0.1.22"]])
+                 [spootnik/kinsky "0.1.22"]]
+  :main sisyphus.core)
