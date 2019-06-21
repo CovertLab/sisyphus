7f3df18073d9a24650402f51ca411d7fd67ace89
Ryan Spangler <ryan.spangler@gmail.com>
2019-06-21 00:31:37 -0700

    adding service file and more gcloud auth steps

--- script/prepare.sh
+++ script/prepare.sh
@@ -11,4 +11,8 @@ curl "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein" >
 chmod +x bin/lein
 export PATH=$HOME/bin:$PATH
 
+git clone https://github.com/CovertLab/sisyphus.git
 
+gcloud auth activate-service-account sisyphus@allen-discovery-center-mcovert.iam.gserviceaccount.com --key-file ~/.cloud.json
+gcloud auth configure-docker
+cat .cloud.json | docker login -u _json_key --password-stdin https://gcr.io
