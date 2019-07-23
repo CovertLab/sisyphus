bc840ace7831109504ef887b9bc911905460ea9b
Ryan Spangler <spanglry@stanford.edu>
2019-07-23 16:48:10 -0700

    working on sisyphus message persistence

--- src/sisyphus/rabbit.clj
+++ src/sisyphus/rabbit.clj
@@ -12,6 +12,11 @@
 (def config-keys
   [:host :port :username :vhost :password])
 
+(def default-config
+  {:routing-key "sisyphus-task"
+   :queue "sisyphus-queue"
+   :exchange "sisyphus-exchange"})
+
 (defn connect!
   "Connect to the rabbitmq service. Accepts a `config` map containing several possible options:
      * :queue - name of the rabbit queue to connect to (default 'sisyphus')
@@ -19,13 +24,15 @@
      * :routing-key - routing key to use for messages (defaults to 'sisyphus')
    Returns a map containing all of the rabbitmq connection information."
   [config]
-  (let [connection (lcore/connect (select-keys config config-keys))
+  (let [config (merge default-config config)
+        connection (lcore/connect (select-keys config config-keys))
         channel (lchannel/open connection)
         _ (lbasic/qos channel 1)
-        queue-name (get config :queue "sisyphus")
-        exchange (get config :exchange "")
-        queue (lqueue/declare channel "sisyphus" {:exclusive false :durable true})
-        routing-key (get config :routing-key "sisyphus")]
+        queue-name (:queue config)
+        exchange (:exchange config)
+        _ (lexchange/declare channel exchange "direct")
+        queue (lqueue/declare channel queue-name {:exclusive false :durable true})
+        routing-key (:routing-key config)]
     (if-not (= exchange "")
       (lqueue/bind channel queue-name exchange {:routing-key routing-key}))
     {:queue queue
