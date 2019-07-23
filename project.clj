ee61f4dd7d890563a50c9fda089b80cf28180de6
Ryan Spangler <ryan.spangler@gmail.com>
2019-07-22 19:32:00 -0700

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sisyphus "0.0.10"
+(defproject sisyphus "0.0.11"
   :description "Eternally execute tasks"
   :url "http://github.com/CovertLab/sisyphus"
   :license {:name "MIT License"
