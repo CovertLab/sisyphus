132361ff7c38d00f8e4240a8283b46b105f6f363
Ryan Spangler <spanglry@stanford.edu>
2019-11-12 13:35:44 -0800

    adding functions for accessing compute services

--- project.clj
+++ project.clj
@@ -9,6 +9,8 @@
                  [com.google.cloud/google-cloud-core "1.87.0"]
                  [com.google.cloud/google-cloud-logging "1.87.0"] ; do not exclude io.grpc/grpc-core io.grpc/grpc-api io.grpc/grpc-netty-shaded or else logging will fail to load classes
                  [com.google.cloud/google-cloud-storage "1.87.0"]
+                 [com.google.apis/google-api-services-compute "v1-rev214-1.25.0"]
+                 ;; [com.google.api-client/google-api-client "1.30.4"]
                  [com.novemberain/langohr "5.1.0"]
                  [spootnik/kinsky "0.1.22"]]
   :pedantic? false
